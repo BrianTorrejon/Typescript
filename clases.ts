9a28685f0268af98d9487b7f96252ab766e7db41
BrianTorrejon <briantorrejon@hotmail.com>
2022-06-02 14:04:29 -0300

    Se implementa herencia, super(), getters y setters

--- clases.ts
+++ clases.ts
@@ -1,10 +1,56 @@
-class Automovil {
-    marca: string;
+class Vehiculo {
+    private readonly marca: string;
+    private modelo: number;
+    private color: string;
+    private precio: string;
 
-    constructor(marca:string){
+    constructor(marca: string, modelo: number, color: string, precio: string) {
         this.marca = marca;
+        this.modelo = modelo;
+        this.color = color;
+        this.precio = precio;
+    }
+
+    /*     constructor(private marca: string, private modelo: number, private color: string) {
+        otra forma de definir constructor,
+        los atributos de arriba se borran
+        } */
+
+    drive() {
+        //this.brandName ="yamaha" <-- no se puede, atributi solo lectura readonly
+        console.log(`conduciendo un ${this.marca}, modelo: ${this.modelo}, color: ${this.color}`);
+    }
+
+    get getPrecio() {
+        return this.precio;
+    }
+
+    set setPrecio(newPrecio: string) {
+        this.precio = newPrecio;
     }
 }
 
-const auto = new Automovil("toyota")
-console.log(auto)
+class Auto extends Vehiculo {
+    private ruedas: number;
+
+    constructor(marca: string, modelo: number, color: string, precio: string, ruedas: number) {
+        super(marca, modelo, color, precio) //super
+        this.ruedas = ruedas;
+    }
+
+    drive(): void {
+        console.log("Conduciendo auto")
+    }
+}
+
+//MAIN
+const auto = new Vehiculo("toyota", 2014, "negro", "200.000");
+auto.drive();
+//auto.marca = "yamaha"; <-- no se puede, atributo privado
+
+console.log(auto.getPrecio)
+auto.setPrecio = "250.000";
+console.log(auto.getPrecio)
+
+const auto2 = new Auto("fiat", 2001, "rojo", "50.000", 4)
+auto2.drive();
